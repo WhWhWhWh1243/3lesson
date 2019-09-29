f557f0257af3bbff0a03b93c842d78e9a0617283
WhWhWhWh1243 <55350554+WhWhWhWh1243@users.noreply.github.com>
2019-09-29 16:30:55 +0300

    Add files via upload

---
+++ 3lesson/fffffffffffffffffff.py
@@ -0,0 +1,121 @@
+'''
+
+for i in range(10):
+    print(i)
+
+
+a = 'Hello world'
+for i in a:
+    print (i)
+
+a = 10
+while a > 5:
+    print(a)
+    a = a - 1
+
+a = 0
+while a < 25:
+    print(a)
+    a = a+1
+
+
+a = 0
+while True:
+    a = a + 1
+    print(a)
+    if a > 10:
+        break
+
+#пользователь ввел число n вывести цикол до n
+a = int(input('введите число '))
+for a in range(a):
+    print(a)
+    
+
+
+
+a = int(input('введите число :  '))
+s = 0
+for i in range(a):
+    s = s+i
+    print(s)
+
+
+a = 0
+n = int(input('введите число :  '))
+while (a+1)   :
+    
+    a = a + 1
+    print(a**2)
+
+a = [1, 2, 3]
+print( type(a) )
+#методы списков
+a.append(5) #ДОБАВИТЬ елемент в конец списка
+.remove(5) #Удаляет првый элеменет списка с выбранным значением
+a.pop(3) #удаляет элемент списка по его индексу
+a.reverse() #разворачивание
+a.count() #считает кол во указанных элементов
+a.clear() #очищает список
+
+
+#Срезы
+print(a[a] )
+
+
+n = []
+for i range (10)
+a = int(input('введите 10 чисел :'))
+
+
+'''
+
+b =[]
+a = int(input())
+d = int(input())
+for i in range(a):
+    b.append(d)
+print(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
